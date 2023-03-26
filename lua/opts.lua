68ecc204c4b0f688bee03758cb1b6cf0cd67f697
Cameron Cunning <negatifzeo@gmail.com>
2023-03-26 09:53:32 -0500

    Add chat GPT and Rust setup

--- lua/opts.lua
+++ lua/opts.lua
@@ -40,3 +40,41 @@ opt.termguicolors = true         -- bool: If term supports ui color then enable
 cmd('colorscheme dracula')       -- cmd:  Set the colorscheme
 
 vim.o.clipboard = "unnamedplus" --Use the system clipboard
+
+
+
+--Options for (rust) completion
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+
+
+-- Treesitter folding 
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+
+
+-- Vimspector options
+vim.cmd([[
+let g:vimspector_sidebar_width = 85
+let g:vimspector_bottombar_height = 15
+let g:vimspector_terminal_maxwidth = 70
+]])
+
--- lua/plug.lua
+++ lua/plug.lua
@@ -30,4 +30,44 @@ return require('packer').startup(function(use)
   use 'voldikss/vim-floaterm' --Terminal.. Just mapping toggle to F7 for now
   use 'tpope/vim-commentary' --Comments
   use 'tpope/vim-surround' --Vim surround!
+  
+  -- Mason. For setting up LSP servers etc.
+  use 'williamboman/mason.nvim'    
+  use 'williamboman/mason-lspconfig.nvim'
+
+  -- A couple of RUST lsp plugins
+  use 'neovim/nvim-lspconfig' 
+  use 'simrat39/rust-tools.nvim'
+
+
+-- Completion framework:
+    use 'hrsh7th/nvim-cmp' 
+
+    -- LSP completion source:
+    use 'hrsh7th/cmp-nvim-lsp'
+
+    -- Useful completion sources:
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    use 'hrsh7th/cmp-vsnip'                             
+    use 'hrsh7th/cmp-path'                              
+    use 'hrsh7th/cmp-buffer'                            
+    use 'hrsh7th/vim-vsnip'                   
+
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'puremourning/vimspector' --Debugger
+-- Chat GPT
+use({
+  "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup({
+        -- optional configuration
+      })
+    end,
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+})
 end)
